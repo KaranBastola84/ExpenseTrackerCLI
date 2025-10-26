bd3b17a49dd605223fb6806701b5c7f725e68e2d
KaranBastola84 <np05cp4a230154@iic.edu.np>
2025-10-26 17:32:38 +0545

    Add Main Program Logic

--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello Karan");
+﻿using ExpenseTracker.Models;
+using ExpenseTracker.Services;
+
+var service = new ExpenseService();
+bool exit = false;
+
+while (!exit)
+{
+    Console.WriteLine("Expense Tracker");
+    Console.WriteLine("1. Add Expense");
+    Console.WriteLine("2. View Expenses");
+    Console.WriteLine("3. Update Expense");
+    Console.WriteLine("4. Delete Expense");
+    Console.WriteLine("5. View Expense Summary");
+    Console.WriteLine("6. Exit");
+    Console.Write("Select an option: ");
+
+    var input = Console.ReadLine();
+    Console.WriteLine();
+
+    switch (input)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+        case "3":
+            break;
+        case "4":
+            break;
+        case "5":
+            break;
+        case "6":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Invalid option. Please try again.");
+            break;
+    }
+
+    Console.WriteLine();
+}
